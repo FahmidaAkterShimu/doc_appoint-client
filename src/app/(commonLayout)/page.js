dcdbb7a219c964be008b1f90ab1e489574dc51df
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-08-08 23:23:55 +0600

    Added Process section

--- src/app/(commonLayout)/page.js
+++ src/app/(commonLayout)/page.js
@@ -1,9 +1,11 @@
 import Appointment from "@/components/Appointment";
+import Process from "@/components/Process";
 import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
     <div>
+      <Process />
       <Testimonials />
       <Appointment />
     </div>
